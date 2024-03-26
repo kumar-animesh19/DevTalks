7f1cb7c2b32b7fea8a2a2e5cf3deba2dfbde81d3
kumar-animesh19 <kumaranimesh1905@gmail.com>
2024-03-26 23:14:28 +0530

    third commit

--- about.php
+++ about.php
@@ -10,12 +10,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <title>Welcome to iDiscuss - Coding Forums</title>
+    <title>Welcome to DevTalks - Coding Forums</title>
 </head>
 
 <body>
     <?php require "partials/_header.php"; ?>
-    
+
     <?php require "partials/_footer.php"; ?>
 
     <!-- Optional JavaScript; choose one of the two! -->
